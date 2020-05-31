c3d91d60fc6d021407301c57d9fe492338fe8540
Ceren Agdas <ceren@fibergames.com.tr>
2020-06-01 01:47:00 +0300

    Added new player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,55 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
-    public List<GameObject> helpers;
-    private Rigidbody rb;
-    private Vector3 inputVector;
 
+    [SerializeField]
+    private List<GameObject> helpers;
+
+    [SerializeField] private Transform endpointTransform;
+    public Vector2 direction;
+    private float editorSpeed = 100.0f;
+    private float speed = 5.0f;
+    private Rigidbody rb;
+    private float velocityX;
+    
+
+    public Vector2 lastPos;
+    
+    private void FixedUpdate()
+    {
+        
+        if (Input.GetMouseButton(0))
+        {
+            float h= Input.GetAxis("Mouse X");
+            float v = Input.GetAxis("Mouse Y");
+
+            Vector3 movement = new Vector3(h, 0 , 0);
+            rb.AddForce(movement * 500);
+            if (h<0)
+            {
+                velocityX = -1f;
+            }
+            else
+            {
+                velocityX = 1f;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            velocityX = 0;
+        }
+
+
+        rb.velocity = new Vector3(velocityX * 3, 0, 1.0f * 10) ;
+        
+    }
     private void MakeInstance()
     {
         if (instance == null)
@@ -20,12 +60,14 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+
         MakeInstance();
     }
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+       // transform.DOMoveZ(endpointTransform.position.z, 15f).SetEase(Ease.Linear);
         SetHelpersActive(true);
     }
 
@@ -37,9 +79,6 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-        inputVector = new Vector3(Input.GetAxis("Horizontal"),rb.velocity.y,0.5f);
-        rb.velocity = inputVector*5;
-    }
+
 }
+
