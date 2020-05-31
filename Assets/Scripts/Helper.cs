9c66580df6280e7a660e9bdfef237ec3679c8ca0
Ceren Agdas <ceren@fibergames.com.tr>
2020-05-31 13:48:01 +0300

    Added player models and created Player base

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Helper : MonoBehaviour
+{
+    public float duration = 0.2f;
+    public int direction = 1;
+    public float rotationAngle=360;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayRotateAnimation();
+    }
+
+    private void PlayRotateAnimation()
+    {
+        transform.DOLocalRotate(new Vector3(0, 0, rotationAngle*direction), duration, RotateMode.Fast)
+            .SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    public List<GameObject> helpers;
+
+    private void MakeInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Awake()
+    {
+        MakeInstance();
+    }
+
+    private void Start()
+    {
+        SetHelpersActive(true);
+    }
+    
+    private void Update()
+    {
+        
+    }
+
+    private void SetHelpersActive(bool isActive)
+    {
+        foreach (var helper in helpers)
+        {
+            helper.SetActive(isActive);
+        }
+    }
+}
