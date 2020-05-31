af2564709a828ce3424b8640b5a45151ac50503e
Ceren Agdas <ceren@fibergames.com.tr>
2020-05-31 16:29:39 +0300

    Created door prefab

---
+++ Assets/Door.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public int direction = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OpenTheDoor();
+    }
+
+    private void OpenTheDoor()
+    {
+        transform.DORotate(new Vector3(0, 0, -90 * direction), 0.4f);
+    }
+}
