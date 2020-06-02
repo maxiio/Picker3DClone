ae6886d0f728005d50c2cdcec79b76ee478d4cc2
Ceren Agdas <ceren@fibergames.com.tr>
2020-06-03 00:57:13 +0300

    Changed player speedX

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private Transform endpointTransform;
 
-    private float _speedX = 3.0f;
+    private float _speedX = 2.0f;
     private float _speedZ = 8.0f;
     private Rigidbody _rb;
     private float _direction;
